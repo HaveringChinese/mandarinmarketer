45959ece467606550607072e182383f34e8ab17a
Nate Conrad <nathanielrconrad@gmail.com>
2021-12-07 23:07:41 -0500

    add new pages and favicon

--- public/js/main.js
+++ public/js/main.js
@@ -37,3 +37,13 @@ axios.request(options).then(function (response) {
 });
 
 
+//worldPopAPI
+
+const Http = new XMLHttpRequest();
+const url = "https://www.worldpop.org/rest/data";
+Http.open("GET", url);
+Http.send();
+
+Http.onreadystatechange=(e)=>{
+  console.log(Http.responseText)
+}
