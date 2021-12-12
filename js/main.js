bff227bc875c69ebdbd087ab69672a853bf234dd
Nate Conrad <nathanielrconrad@gmail.com>
2021-12-12 17:15:52 -0500

    add dynamic copyright

---
+++ js/main.js
@@ -0,0 +1,21 @@
+
+var year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
+//dropdown menu
+
+function dropDown() {
+  document.getElementById("menu-drop-down").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}; 
