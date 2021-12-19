5784db8672f0c826e2f3532067b063bb044b9d0e
Nate Conrad <nathanielrconrad@gmail.com>
2021-12-19 14:50:07 -0500

    link clients and process pages

--- src/main.js
+++ src/main.js
@@ -3,26 +3,12 @@ var year = new Date().getFullYear();
 document.getElementById("year").innerHTML = year;
 //dropdown menu
 
-  var angloPop = 379004765;
-  var sinoPop = 918573829;
-  var secs = 230;
 
-  document.getElementById("anglo-pop").innerHTML = angloPop.toLocaleString("en");
-  document.getElementById("sino-pop").innerHTML = sinoPop.toLocaleString("en");
-
-setInterval(function(){
-  angloPop++;
-  sinoPop++;
-  document.getElementById("anglo-pop").innerHTML = angloPop.toLocaleString("en");
-  document.getElementById("sino-pop").innerHTML = sinoPop.toLocaleString("en");
-}, secs);
-
-
-
-
-function dropDown() {
-  document.getElementById("menu-drop-down").classList.toggle("show");
-}
+window.onload = function dropDown() {
+  document.getElementById("dropbtn").addEventListener('click', function() {
+    document.getElementById("menu-drop-down").classList.toggle("show");  
+  });
+};
 
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
@@ -36,3 +22,33 @@ window.onclick = function(event) {
     }
   }
 }; 
+
+// function emailSubmitted(){
+//   document.getElementById("take-email").replaceChildren()
+// }
+
+
+// <input id='btnRemoveDummy' type="button" value="Remove DUMMY"/>
+
+
+// function removeDummy() {
+//     var elem = document.getElementById('dummy');
+//     elem.parentNode.removeChild(elem);
+//     return false;
+// }
+// function pageInit() {
+//     // Hook up the "remove dummy" button
+//     var btn = document.getElementById('btnRemoveDummy');
+//     if (btn.addEventListener) {
+//         // DOM2 standard
+//         btn.addEventListener('click', removeDummy, false);
+//     }
+//     else if (btn.attachEvent) {
+//         // IE (IE9 finally supports the above, though)
+//         btn.attachEvent('onclick', removeDummy);
+//     }
+//     else {
+//         // Really old or non-standard browser, try DOM0
+//         btn.onclick = removeDummy;
+//     }
+// }
