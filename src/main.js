524d0132129bdaea237210adab1c60cb8f9478e3
Nate Conrad <nathanielrconrad@gmail.com>
2021-12-20 19:36:05 -0500

    finish clients page, consolidate js, begin process page

--- src/main.js
+++ src/main.js
@@ -1,14 +1,10 @@
-
-var year = new Date().getFullYear();
-document.getElementById("year").innerHTML = year;
-//dropdown menu
-
-
-window.onload = function dropDown() {
+function dropDown() {
   document.getElementById("dropbtn").addEventListener('click', function() {
     document.getElementById("menu-drop-down").classList.toggle("show");  
   });
-};
+}
+
+dropDown();
 
 window.onclick = function(event) {
   if (!event.target.matches('.dropbtn')) {
@@ -22,6 +18,49 @@ window.onclick = function(event) {
     }
   }
 }; 
+
+if (document.body.classList.contains("main-page")){
+  populationIncrease();
+}
+
+var angloPop = 379004765;
+var sinoPop = 918573829;
+var secs = 230;
+
+function populationIncrease(){
+  document.getElementById("anglo-pop").innerHTML = angloPop.toLocaleString("en");
+  document.getElementById("sino-pop").innerHTML = sinoPop.toLocaleString("en");
+
+  setInterval(function(){
+    angloPop++;
+    sinoPop++;
+    document.getElementById("anglo-pop").innerHTML = angloPop.toLocaleString("en");
+    document.getElementById("sino-pop").innerHTML = sinoPop.toLocaleString("en");
+}, secs);
+}
+
+
+var year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
+//dropdown menu
+
+console.log("this works!");
+
+function showMore(){
+  var hiddenThings = document.getElementsByClassName("paragraph-body");
+  for(var thing in hiddenThings){
+    thing.classList.toggle("show");
+  }
+}
+
+var readMore = document.getElementById("first");
+readMore.addEventListener("click", function() {
+    console.log("did we make it?");
+    console.log(readMore);
+    showMore();
+});
+
+
 
 // function emailSubmitted(){
 //   document.getElementById("take-email").replaceChildren()
